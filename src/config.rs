f1a2b62bd729ccb052c484eec0da2d9623787a41
nicolasito1411 <60229704+Marchand-Nicolas@users.noreply.github.com>
2023-05-20 13:00:35 +0200

    update

--- src/config.rs
+++ src/config.rs
@@ -3,8 +3,8 @@ pub struct Config {
     pub database_url: String,
     pub database_name: String,
     pub jwt_secret: String,
-    pub jwt_expires_in: String,
-    pub jwt_maxage: i32,
+    pub jwt_user_maxage: i32,
+    pub password_salt: String,
 }
 
 impl Config {
@@ -12,14 +12,15 @@ impl Config {
         let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
         let database_name = std::env::var("DATABASE_NAME").expect("DATABASE_NAME must be set");
         let jwt_secret = std::env::var("JWT_SECRET").expect("JWT_SECRET must be set");
-        let jwt_expires_in = std::env::var("JWT_EXPIRED_IN").expect("JWT_EXPIRED_IN must be set");
-        let jwt_maxage = std::env::var("JWT_MAXAGE").expect("JWT_MAXAGE must be set");
+        let jwt_user_maxage =
+            std::env::var("JWT_USER_MAX_AGE").expect("JWT_USER_MAX_AGE must be set");
+        let password_salt = std::env::var("PASSWORD_SALT").expect("PASSWORD_SALT must be set");
         Config {
             database_url,
             database_name,
             jwt_secret,
-            jwt_expires_in,
-            jwt_maxage: jwt_maxage.parse::<i32>().unwrap(),
+            jwt_user_maxage: jwt_user_maxage.parse::<i32>().unwrap(),
+            password_salt: std::env::var("PASSWORD_SALT").expect("PASSWORD_SALT must be set"),
         }
     }
 }
--- src/route.rs
+++ src/route.rs
@@ -1,14 +1,19 @@
 use std::sync::Arc;
 
 use axum::{
+    middleware,
     routing::{get, post},
     Router,
 };
 
-use crate::{handler::health_checker_handler, AppState};
+use crate::{handler::health_checker_handler, handler::login, AppState};
 
 pub fn create_router(app_state: Arc<AppState>) -> Router {
     Router::new()
-        .route("/api/healthchecker", get(health_checker_handler))
+        .route("/health_checker", get(health_checker_handler))
+        .route(
+            "/login",
+            post(login).route_layer(middleware::AddExtension::new(app_state.clone())),
+        )
         .with_state(app_state)
 }
