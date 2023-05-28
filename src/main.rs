12f195ac9a3b14b7f15d54cf409e6c7a9cc8530c
nicolasito1411 <60229704+Marchand-Nicolas@users.noreply.github.com>
2023-05-28 21:30:12 +0200

    updating cors

--- src/main.rs
+++ src/main.rs
@@ -51,7 +51,7 @@ async fn main() {
     }
 
     let cors = CorsLayer::new()
-        .allow_origin("http://localhost:3000".parse::<HeaderValue>().unwrap())
+        .allow_origin("*".parse::<HeaderValue>().unwrap())
         .allow_methods([Method::GET, Method::POST, Method::PATCH, Method::DELETE])
         .allow_credentials(true)
         .allow_headers([AUTHORIZATION, ACCEPT, CONTENT_TYPE]);
